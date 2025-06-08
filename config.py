c5a1981b34f6dafc3e371ffd0c8d772d8198d6f5
Syed NoorAlam <160272348+FcnAshir@users.noreply.github.com>
2025-06-08 06:45:06 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 import os
 
 # Set your OpenAI API key
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "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")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "insert own keys
